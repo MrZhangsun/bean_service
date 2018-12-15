ab362eeb463011d646821ddb1993cd51a3a73c84
zhangsun.jiankun <mr_zhangsun@outlook.com>
2018-12-16 00:43:34 +0800

    init backend service project

---
+++ src/main/java/top/zhangsun/bean/BeanServiceApplication.java
@@ -0,0 +1,14 @@
+package top.zhangsun.bean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanServiceApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/top/zhangsun/bean/BeanServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package top.zhangsun.bean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeanServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
